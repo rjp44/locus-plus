35fb786ad9c0e579be615f4ba457347be23e9a25
Rob Pickering <rob@pickering.org>
2021-05-06 22:49:21 +0100

    Tidy up Location class

--- src/components/PhoneticPlus.jsx
+++ src/components/PhoneticPlus.jsx
@@ -2,33 +2,38 @@ import React, { useState, useEffect } from 'react';
 import Location from '../lib/location';
 
 function PhoneticPlus(props) {
-    const [location, setLocation] = useState(
-        { phoneticCode: 'Dont know yet, please allow location access' }
-    );
+  const [location, setLocation] = useState(
+    { phoneticCode: 'Dont know yet, please allow location access' }
+  );
 
-    useEffect(() => {
-        // When we are mounted, instantiate a location object and update state when we get a value
-        let l = new Location();
-        l.position
-            .then(position => setLocation({ ...position }))
-            .catch(err => setLocation({ err }))
-    }, []);
+  useEffect(() => {
+    // When we are mounted, instantiate a location object and update state when we get a value
+    let lcn = new Location();
+    lcn.queryDevice().then(() => {
+      setLocation({
+        phoneticCode: lcn.phoneticCode,
+        plusCode: lcn.plusCode,
+        phoneticCodes: lcn.phoneticCodes(5)
+      });
+    })
+      .catch(err => setLocation({ err }));
+  }, []);
 
-    return (
-        <div>
-            <h1>You are at</h1>
-            <p key="phonetic">{location.phoneticCode || location.err}</p>
-            <h2>AKA</h2>
-            {
-            location.phoneticCodes && location.phoneticCodes
-                .filter((code, index) => index > 0)
-                .map((code, index) =>
-                    (<p key={index}>{code}</p>))
-            }
-            <h3>or Just</h3>
-            <p>{location.plusCode}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>You are at</h1>
+      <p key="phonetic">{location.phoneticCode || location.err}</p>
+      <h2>AKA</h2>
+      {
+        location.phoneticCodes && location.phoneticCodes
+          .filter((code, index) => index > 0)
+          .map((code, index) =>
+            (<p key={index}>{code}</p>))
+      }
+      <h3>or Just</h3>
+      <p>{location.plusCode}</p>
+    </div>
+  );
 
 }
 
--- src/lib/location.js
+++ src/lib/location.js
@@ -1,82 +1,124 @@
 import OpenLocationCode from "open-location-code/js/src/openlocationcode.js";
-import phonetic from 'alpha-bravo';
-import { decompress} from 'compress-json'
+import phonetic from "alpha-bravo";
+import {
+  decompress
+} from "compress-json";
 
-const places = decompress(require('../places.json'));
-
+const places = decompress(require("../places.json"));
 
 export default class Location {
 
-    constructor() {
-        let geolocation = navigator.geolocation || global.navigator.geolocation;
-        this.position = new Promise((resolve, reject) => 
-            geolocation.getCurrentPosition(position => {
-                this.build(position.coords);
-                resolve(this);
-            }, 
-                err => reject(err),
-                { enableHighAccuracy: true }
-            )
+
+  constructor(latitude, longitude, altitude) {
+    this._position = latitude != null && longitude != null && { latitude, longitude, altitude };
+  }
+
+
+  get plusCode() {
+    if (!this._position?.latitude)
+      return undefined;
+
+    return this._plusCode || (this._pluscode = OpenLocationCode.encode(
+      this._position.latitude,
+      this._position.longitude,
+      OpenLocationCode.CODE_PRECISION_EXTRA
+    ));
+  }
+
+  get shortCode() {
+    if (!this._position?.latitude)
+      return undefined;
+    if (!this._shortCodes)
+      this._buildShortCodes(this._position);
+    return this._shortCodes[0];
+  }
+
+  get phoneticCode() {
+    return this.shortCode && this._phoneticCodes[0];
+  }
+
+
+  queryDevice() {
+    let geolocation = navigator.geolocation || global.navigator.geolocation;
+    if (!this.position)
+      this.position = new Promise((resolve, reject) =>
+        geolocation.getCurrentPosition(
+          (location) => resolve(location.coords),
+          (err) => reject(err), {
+          enableHighAccuracy: true,
+        }
         )
-     }
+      )
+        .then(({ latitude, longitude, altitude }) => (this._position = { latitude, longitude, altitude }));
+    return this.position;
+  }
 
-    build(position) {
-        this.phoneticCodes = [];
-        this.shortCodes = [];
-        let {
-            latitude,
-            longitude,
-            altitude
-        } = position;
-        let plusCode = OpenLocationCode.encode(latitude, longitude, OpenLocationCode.CODE_PRECISION_EXTRA);
-        let seenCode = {};
+  shortCodes(num = 5) {
+    return this.shortCode && this._shortCodes.slice(0, num);
+  }
 
-        Object.assign(this, {
-            latitude,
-            longitude,
-            altitude,
-            plusCode
-        });
-        let references = [
-            plusCode.slice(0, 4),
-            plusCode.slice(0, 6),
-            plusCode.slice(0, 8),
-            plusCode.slice(0, 11)
-        ]
-            .reduce((o, code) => o.concat(places[code] || []), [])
-            .map(r => ({
-                ...r,
-                distance: (r.lat - latitude) ** 2 + (r.long - longitude) ** 2,
-                shortCode: OpenLocationCode.shorten(this.plusCode, r.lat, r.long)
-            }))
-            .filter(r => r.shortCode.length !== plusCode.length)
-            .sort((a, b) => {
-               let res = a.shortCode.length - b.shortCode.length;
-               if (res === 0)
-                 res = a.distance - b.distance;
-              if (res === 0)
-                 res = b.hierarchy - a.hierarchy;
-              return (res);
-            })
-            .map(r => ({
-                ...r,
-                shortCode: r.shortCode.trim() + `, ${r.name} ${r.country}`,
-                phoneticCode: phonetic.returnAsString(r.shortCode).replace(/\+/, 'plus').trim() + `, ${r.name}, ${r.country}`
-            }))
-  
-            .filter(r => !(seenCode[r.shortCode] || !(seenCode[r.shortCode] = true)))
-     
-        let [shortCodes, phoneticCodes] = [references.map(r => r.shortCode).slice(0, 5), references.map(r => r.phoneticCode).slice(0, 5)];
-        Object.assign(this, { shortCodes, phoneticCodes });
+  phoneticCodes(num = 5) {
+    return this.shortCode && this._phoneticCodes.slice(0, num);
+  }
 
-        this.shortCodes.push(this.plusCode)
-        this.phoneticCodes.push(phonetic.returnAsString(this.plusCode).replace(/\+/, 'plus'));
-        // Primary short and phonetic codes are the longest prefixes we found first.
-        this.shortCode = this.shortCodes[0];
-        this.phoneticCode = this.phoneticCodes[0];
+  _buildShortCodes(position) {
+    let {
+      latitude,
+      longitude,
+      altitude
+    } = position;
+    let plusCode = this.plusCode;
+    let seenCode = {};
 
-    
 
-    }
- 
+    let references = [
+      plusCode.slice(0, 4),
+      plusCode.slice(0, 6),
+      plusCode.slice(0, 8),
+      plusCode.slice(0, 11),
+    ]
+      // All of the places in all of the above arrays
+      .reduce((o, code) => o.concat(places[code] || []), [])
+      .map((r) => ({
+        ...r,
+        // Dont bother with the square root calc, we just want to know which distances are smaller not by how much
+        distance: (r.lat - latitude) ** 2 + (r.long - longitude) ** 2,
+        shortCode: OpenLocationCode.shorten(this.plusCode, r.lat, r.long),
+      }))
+      .filter((r) => r.shortCode.length !== plusCode.length)
+      .sort((a, b) => {
+        // Primary sort criteria is shortcode length, then distance, then notability
+        let res = a.shortCode.length - b.shortCode.length;
+        res = res || a.distance - b.distance;
+        res = res || b.hierarchy - a.hierarchy;
+        return res;
+      })
+      // Add reference place name
+      .map((r) => ({
+        ...r,
+        shortCode: r.shortCode.trim() + `, ${r.name} ${r.country}`,
+        phoneticCode: phonetic.returnAsString(r.shortCode).replace(/\+/, "plus").trim() +
+          `, ${r.name}, ${r.country}`,
+      }))
+      // remove dups
+      .filter(
+        (r) => !(seenCode[r.shortCode] || !(seenCode[r.shortCode] = true))
+      );
+
+    let [shortCodes, phoneticCodes] = [
+      references.map((r) => r.shortCode),
+      references.map((r) => r.phoneticCode),
+    ];
+    // Push the full code as a backstop.
+    shortCodes.push(this.plusCode);
+    phoneticCodes.push(
+      phonetic.returnAsString(this.plusCode).replace(/\+/, "plus")
+    );
+    Object.assign(this, {
+      _shortCodes: shortCodes,
+      _phoneticCodes: phoneticCodes,
+      _shortCode: shortCodes[0],
+      _phoneticCode: phoneticCodes[0],
+    });
+  }
 }
